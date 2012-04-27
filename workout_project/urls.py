e6955fb4dff0608044f0e114a2362a7dbd8e9aee
Karan Sagar <ksagar1030@gmail.com>
2012-04-27 03:26:03 -0400

    fleshed out detail view

--- workout_project/urls.py
+++ workout_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^$', 'workouts_app.views.home', name='home'),
 		
-    url(r'u/(?P<uid>\d+)/(?P<wkoutname>)/$', 'workouts_app.views.wkdetail'),
+    url(r'u/(?P<uid>\d+)/(?P<wkoutname>\w+)/$', 'workouts_app.views.wkdetail'),
 		
 	# Examples:
     # url(r'^workout_project/', include('workout_project.foo.urls')),
--- workouts_app/views.py
+++ workouts_app/views.py
@@ -12,4 +12,7 @@ def home(request):
 	return render_to_response('workouts/index_temp.html')	
 
 def wkdetail(request, uid, wkoutname):
-	pass
+	u = get_object_or_404(User, pk=uid)
+	w = get_object_or_404(Workout,user=u , name=wkoutname)
+	d = []
+	return render_to_response('workouts/wkoutdetail.html', {'workout':w, })
